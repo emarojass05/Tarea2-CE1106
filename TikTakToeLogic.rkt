ca1323c6a7c0290835804ecc68ed54df5edb8889
MrGabo06 <ggabriel0635@gmail.com>
2024-09-03 15:26:07 -0600

    [ADD]: Se crea la funcion para crear matriz

--- TikTakToeLogic.rkt
+++ TikTakToeLogic.rkt
@@ -1,5 +1,18 @@
 #lang racket
-(define (TTT i j)
-  (display i)
-  (newline)
-  (display j))
+
+(define (TTT m n)
+  (cond ((< m 3)(display "El minimo de la matriz es 3x3"))
+        ((< n 3)(display "El minimo de la matriz es 3x3"))
+        ((> n 10)(display "El maximo de la matriz es 10x10"))
+        ((> m 10)(display "El maximo de la matriz es 10x10"))
+        (else (createMat m n))))
+
+(define (createMat m n)
+    (if (= m 0)
+        '()
+        (cons (createRow n) (createMat (- m 1) n))))
+
+(define (createRow n)
+    (if (= n 0)
+        '()
+        (cons 0 (createRow (- n 1)))))
